6349d293f67da5f5ad776149abdc9bcff098e5cc
Youenn Denécé <youenn.denece@gmail.com>
2022-11-18 23:17:25 +0100

    new branch progress

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('short'));
 
-app.get('/', (req, res) => {
-    res.end('index');
-})
-app.post('/', (req, res) => {
-    let body = '';
-    req.on('data',(data) => {
-        body += data;
-    })
-req.on('end', () => {
-    console.log(body);
-    console.log(typeof body);
-    res.end('index');
-})
 
-    
-})
 
 app.listen(3000);
