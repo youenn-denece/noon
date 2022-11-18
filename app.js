e71f80210b3b974a9ead9f34cc795a7716839f95
Youenn Denécé <youenn.denece@gmail.com>
2022-11-18 23:16:03 +0100

    init project noon

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('short'));
 
 app.get('/', (req, res) => {
-    res.writeHead(200, {
-        'content-type':'text/plain'
-    });
+    res.end('index');
+})
+app.post('/', (req, res) => {
+    let body = '';
+    req.on('data',(data) => {
+        body += data;
+    })
+req.on('end', () => {
+    console.log(body);
+    console.log(typeof body);
+    res.end('index');
+})
 
-    res.end('Hello');
+    
 })
 
 app.listen(3000);
